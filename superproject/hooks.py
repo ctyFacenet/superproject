2d1d696e441bdf4a754c42261a9daac5361d64c4
The Citizen <kienthanh2seri@gmail.com>
2025-11-10 03:41:01 +0000

    update change logo defaut

--- superproject/hooks.py
+++ superproject/hooks.py
@@ -4,6 +4,7 @@ app_publisher = "FaceNet"
 app_description = "FaceNet"
 app_email = "contact@facenet.vn"
 app_license = "mit"
+app_logo_url = "/assets/superproject/images/logo_fn.png"
 
 # Apps
 # ------------------
@@ -47,6 +48,9 @@ web_include_css = [
 ]
 # web_include_js = "/assets/superproject/js/superproject.js"
 
+website_context = {
+	"splash_image": "/assets/superproject/images/logo_fn.png",
+}
 # include custom scss in every website theme (without file extension ".scss")
 # website_theme_scss = "superproject/public/scss/website"
 
