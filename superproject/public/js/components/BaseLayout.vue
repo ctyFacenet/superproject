a24ffee3b0785eda7519528ceed98f9879853d17
The Citizen <kienthanh2seri@gmail.com>
2025-11-28 08:05:11 +0000

    update: css

--- superproject/public/js/components/BaseLayout.vue
+++ superproject/public/js/components/BaseLayout.vue
@@ -68,7 +68,7 @@
                 </div>
 
                 <a-input placeholder="Nhập thông tin để tìm kiếm"
-                  class="sm:tw-w-[220px] md:tw-w-[260px] lg:tw-w-[300px] tw-h-[30px] tw-text-[13px] tw-rounded-sm tw-border-[#0ba5ec] focus:tw-shadow-none tw-flex-shrink-0"
+                  class="sm:tw-w-[220px] md:tw-w-[260px] lg:tw-w-[300px] tw-h-[30px] tw-text-[13px] tw-rounded-sm focus:tw-shadow-none tw-flex-shrink-0"
                   size="small" allowClear>
                   <template #prefix>
                     <SearchOutlined class="tw-text-gray-400" />
